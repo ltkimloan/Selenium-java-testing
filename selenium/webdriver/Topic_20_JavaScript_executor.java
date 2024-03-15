1bdaaeff81b53d4f145b4756191ff0dfdb7978d2
Le Thi Kim Loan <ltkimloankg99@gmail.com>
2024-03-15 18:01:56 +0700

    update TC 03 Create Account

--- selenium/webdriver/Topic_20_JavaScript_executor.java
+++ selenium/webdriver/Topic_20_JavaScript_executor.java
@@ -5,11 +5,13 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.Random;
 
 public class Topic_20_JavaScript_executor {
 	WebDriver driver;
@@ -27,20 +29,84 @@ public class Topic_20_JavaScript_executor {
 
 	}
 	@Test
-	public void TC_01_() {
+	public void TC_01_javaScript_Executor() {
+	navigateToUrlByJS("http://live.techpanda.org/");
+	String verifyDomain = (String) executeForBrowser("return document.domain;");
+	Assert.assertEquals(verifyDomain,"live.techpanda.org");
 
+	String verifyUrl = (String) executeForBrowser("return document.URL;");
+	Assert.assertEquals(verifyUrl,"http://live.techpanda.org/");
 
+	clickToElementByJS("//a[text()='Mobile']");
+	sleepInSecond(2);
+
+	clickToElementByJS("//a[@title='Samsung Galaxy']/parent::h2/following-sibling::div[@class='actions']//span[text()='Add to Cart']");
+	sleepInSecond(2);
+
+	Assert.assertTrue(getInnerText().contains("Samsung Galaxy was added to your shopping cart."));
+
+	clickToElementByJS("//a[text()='Customer Service']");
+	sleepInSecond(2);
+
+	String customerTitle = (String) executeForBrowser("return document.title;");
+	Assert.assertEquals(customerTitle,"Customer Service");
+
+	scrollToBottomPage();
+	sendkeyToElementByJS("//input[@id='newsletter']",getEmailAddress());
+
+	clickToElementByJS("//button[@title='Subscribe']");
+	sleepInSecond(2);
+
+	Assert.assertTrue(isExpectedTextInInnerText("Thank you for your subscription."));
+	}
+
+	@Test
+	public void TC_02_html5_messages() {
+	driver.get("https://sieuthimaymocthietbi.com/account/register");
+	sleepInSecond(2);
+
+	driver.findElement(By.xpath("//button[text()='Đăng ký']")).click();
+
+	Assert.assertEquals(getElementValidationMessage("//input[@id='lastName']"),"Please fill out this field.");
+
+	driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("Auto");
+	driver.findElement(By.xpath("//button[text()='Đăng ký']")).click();
+	sleepInSecond(2);
+	Assert.assertEquals(getElementValidationMessage("//input[@id='firstName']"),"Please fill out this field.");
+
+	driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("Testing");
+	driver.findElement(By.xpath("//button[text()='Đăng ký']")).click();
+	sleepInSecond(2);
+	Assert.assertEquals(getElementValidationMessage("//input[@id='email']"),"Please fill out this field.");
+
+	driver.findElement(By.xpath("//input[@id='email']")).sendKeys("au@ee@ee");
+	driver.findElement(By.xpath("//button[text()='Đăng ký']")).click();
+	sleepInSecond(5);
+	Assert.assertEquals(getElementValidationMessage("//input[@id='email']"),"Please enter an email address.");
 
 	}
 
 	@Test
-	public void TC_02_() {
+	public void TC_03_Create_An_Account() {
+	navigateToUrlByJS("http://live.techpanda.org/");
+	clickToElementByJS("//div[@id='header-account']//a[@title='My Account']");
+	clickToElementByJS("//a[@title='Create an Account']");
+	String firstName = "Auto", lastName = "testing", password = "123456";
 
-	}
+	sendkeyToElementByJS("//input[@id='firstname']",firstName);
+	sendkeyToElementByJS("//input[@id='lastname']",lastName);
+	sendkeyToElementByJS("//form[@id='form-validate']//input[@type='email']",getEmailAddress());
+	sendkeyToElementByJS("//form[@id='form-validate']//input[@id='password']",password);
+	sendkeyToElementByJS("//form[@id='form-validate']//input[@id='confirmation']",password);
 
-	@Test
-	public void TC_03_() {
+	clickToElementByJS("//button[@title='Register']");
+	sleepInSecond(5);
 
+	Assert.assertTrue(isExpectedTextInInnerText("Thank you for registering with Main Website Store."));
+	clickToElementByJS("//div[@id='header-account']//a[text()='Log Out']");
+	sleepInSecond(6);
+
+	Assert.assertTrue(isImageLoaded("//div[@class='page-title']//img"));
 	}
 
 	public void sleepInSecond(long timeInSecond) {
@@ -51,6 +117,10 @@ public class Topic_20_JavaScript_executor {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	public String getEmailAddress() {
+		Random rand = new Random();
+		return "mikejoin" + rand.nextInt(9999) + "@gmail.net";
 	}
 
 	@AfterClass
